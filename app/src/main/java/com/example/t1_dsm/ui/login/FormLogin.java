4bfb03f058920b83344254cdc53bf38fa4f9609b
Vanderlei Junior <vanderlei@estudante.ufscar.br>
2022-08-04 00:26:05 -0300

    [Java] Profile

--- app/src/main/java/com/example/t1_dsm/ui/login/FormLogin.java
+++ app/src/main/java/com/example/t1_dsm/ui/login/FormLogin.java
@@ -44,7 +44,6 @@ public class FormLogin extends AppCompatActivity {
 
         // Carregar idioma salvo
         Locale currentLocale = LoadSharedPreferences();
-
         Locale.setDefault(currentLocale);
         Resources resources = this.getResources();
         Configuration configuration = resources.getConfiguration();
@@ -102,7 +101,7 @@ public class FormLogin extends AppCompatActivity {
             SharedPreferences.Editor editor = preferences.edit();
             editor.putString("localeLanguage", selectedLocale.getLanguage());
             editor.putString("localeCountry", selectedLocale.getCountry());
-            editor.commit();
+            editor.apply();
 
             // Aplicar as Configs
             recreate();
